30119e06307c80293460b3b9b6ecf7fa48d069b8
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-02-17 18:53:39 +0530

    added homepage

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="page-container">
+      <div className={styles.main}>
+        <h1 className="">Next.js News App</h1>
+        <h3 className="">Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
+  );
 }
