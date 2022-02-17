290eb32958fba6312980c67f2a9df3cbcb5e3687
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-02-17 18:52:03 +0530

    added eom page

---
+++ pages/eom.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from '../styles/EOM.module.css'
+
+export const eom = ({ employee }) => {
+  console.log(employee);
+  return (
+    <div className="page-container">
+      <div className={styles.main}>
+        <h1 className="">Employee of the Month </h1>
+        <div className={styles.employeeOfTheMonth}>
+            <h3 className="">{employee.name}</h3>
+            <h6 className="">{employee.position}</h6>
+            <img src={employee.image} alt="" className="" />
+            <p className="">{employee.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth"
+  );
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee,
+    },
+  };
+};
+
+export default eom;
--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <>Hello World!</>;
+  return <div>Hello World!</div>;
 }
